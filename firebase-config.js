b53ee7b29ed7500430e2e39ed5dda83f34940e7d
2402081193 <2402081193@siswa.icats.edu.my>
2026-03-09 10:25:32 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase configuration (REPLACE WITH YOUR OWN)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAOoM3qgNaV2HrxRBAmrkcJswSu_QHTsy0",
+  authDomain: "online-banking-system1.firebaseapp.com",
+  projectId: "online-banking-system1",
+  storageBucket: "online-banking-system1.firebasestorage.app",
+  messagingSenderId: "411135411782",
+  appId: "1:411135411782:web:96dc73b1fcf1a9688d2c47"
 };
 
 // Initialize Firebase
